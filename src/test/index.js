cf266601c4da1afc8838a6b73a08ffb3d65b5db7
Didier Plaindoux <d.plaindoux@free.fr>
2017-09-09 09:30:52 +0200

    Compile ready for talk

--- src/test/index.js
+++ src/test/index.js
@@ -9,12 +9,10 @@ import parserExpressionsTest  from './lang/analyzer/parser_expressions_test'
 import parserEntitiesTest     from './lang/analyzer/parser_entities_test'
 import deBruijnExpressionTest from './lang/compiler/debruijn_expression_test'
 import objcodeExpressionTest  from './lang/compiler/objcode_expression_test'
-import machineTest            from './lang/runtime/machine_test'
 
 export {
     parserExpressionsTest,
     parserEntitiesTest,
     deBruijnExpressionTest,
-    objcodeExpressionTest,
-    machineTest
+    objcodeExpressionTest
 }
