2766a15f36b02de3f7015041d2c9e16934772ca8
Didier Plaindoux <d.plaindoux@free.fr>
2017-09-09 09:27:10 +0200

    Transform ready for talk

--- src/test/index.js
+++ src/test/index.js
@@ -8,11 +8,9 @@
 import parserExpressionsTest  from './lang/analyzer/parser_expressions_test'
 import parserEntitiesTest     from './lang/analyzer/parser_entities_test'
 import deBruijnExpressionTest from './lang/compiler/debruijn_expression_test'
-import objcodeExpressionTest  from './lang/compiler/objcode_expression_test'
 
 export {
     parserExpressionsTest,
     parserEntitiesTest,
-    deBruijnExpressionTest,
-    objcodeExpressionTest
+    deBruijnExpressionTest
 }
